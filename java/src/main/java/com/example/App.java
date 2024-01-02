d69e2ad623550a41f92747eb915d8f0923d52832
coryo12345 <corydio12@gmail.com>
2024-01-01 21:14:37 -0500

    remove warning

--- java/src/main/java/com/example/App.java
+++ java/src/main/java/com/example/App.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.util.Optional;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -11,7 +9,5 @@ public class App {
         list.add("b");
         list.add("c");
         System.out.println(list.toString((String value) -> value));
-
-        Optional<String> a = list.get(0);
     }
 }
